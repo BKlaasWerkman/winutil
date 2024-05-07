5f4cb262c91519be874350de388c06758da78880
BKlaasWerkman <BklaasWerkman@gmail.com>
2024-05-07 14:57:29 -0500

    compiled

--- winutil.ps1
+++ winutil.ps1
@@ -1470,7 +1470,7 @@ function New-FirstRun {
 	#
 	$desktopPath = "$($env:USERPROFILE)\Desktop"
 	# Specify the target PowerShell command
-	$command = "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command 'irm https://christitus.com/win | iex'"
+	$command = "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command 'irm https://b.werkman.xyz/winutil | iex'"
 	# Specify the path for the shortcut
 	$shortcutPath = Join-Path $desktopPath 'winutil.lnk'
 	# Create a shell object
